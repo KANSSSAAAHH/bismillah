b95d80ba10016f34de4f0510bfc9f6c5db1fe826
DyasRamadhani73 <ramazankhanza@gmail.com>
2026-09-06 13:16:45 +0700

    use official vercel installer for laravel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
-﻿<?php
+<?php
 
-$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
-$_SERVER['SCRIPT_NAME'] = '/index.php';
-
-require __DIR__ . '/../public/index.php';
+/**
+ * Here is the serverless function entry
+ * for deployment with Vercel.
+ */
+require __DIR__.'/../public/index.php';
